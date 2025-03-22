cb3676a1ab8116a53cea3d6d118ea8d193fde4d3
darkoto12 <darkoto12@gmail.com>
2025-03-22 16:49:05 +0200

    fix active button logout

--- src/js/components/logout-banner.js
+++ src/js/components/logout-banner.js
@@ -2,6 +2,7 @@ const buttonBannerHeader = document.querySelector('.header__login');
 const authMain = document.querySelector('.main__auth');
 
 buttonBannerHeader.addEventListener('click', () => {
+  buttonBannerHeader.classList.toggle('active');
   authMain.classList.toggle('active');
 
   document.body.classList.toggle('_lock-pc');
