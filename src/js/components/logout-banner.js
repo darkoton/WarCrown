63ad94324980837bef5a4e8338fb092114afce94
darkoto12 <darkoto12@gmail.com>
2025-03-22 16:31:40 +0200

    add auth form in main block

---
+++ src/js/components/logout-banner.js
@@ -0,0 +1,7 @@
+const buttonBannerHeader = document.querySelector('.header__login');
+const authMain = document.querySelector('.main__auth');
+
+buttonBannerHeader.addEventListener('click', () => {
+  authMain.classList.toggle('active');
+  document.body.classList.toggle('_lock-scroll');
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 @@include("./modules/swiper.js"); // SLIDER SWIPER 
 
 @@include("./components/header-feed.js"); //HEADER FEED
+
+@@include("./components/logout-banner.js"); //HEADER FEED
